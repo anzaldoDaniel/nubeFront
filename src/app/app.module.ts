75a2dc2b880c268b085ef977fbbbdadad7525763
Joaquin Ojeda <joaquin_ema_ojeda@hotmail.com>
2024-06-08 17:01:15 -0300

    Agrego pantalla de Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
+import { InicioComponent } from './screens/inicio/inicio.component';
+import { TareaCardComponent } from './components/tarea-card/tarea-card.component';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    TareaCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
