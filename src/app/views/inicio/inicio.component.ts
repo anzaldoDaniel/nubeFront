8b403f54649dab831ce09328664b0b3852240eaf
Joaquin Ojeda <joaquin_ema_ojeda@hotmail.com>
2024-06-12 21:16:34 -0300

    Arreglos finales

--- src/app/views/inicio/inicio.component.ts
+++ src/app/views/inicio/inicio.component.ts
@@ -10,6 +10,7 @@ import { ApiTareasService } from 'src/app/services/api-tareas.service';
 })
 export class InicioComponent {
   tareas: Tarea[]=[];
+  tareasTotal: Tarea[]=[];
   cargando: Boolean = true;
   error: Boolean = false;
   @ViewChild('errorModal') errorModal?: ElementRef;
@@ -17,8 +18,13 @@ export class InicioComponent {
   constructor(private apiTareasService: ApiTareasService){}
 
   ngOnInit(){
+    this.cargarTareas();
+  }
+
+  cargarTareas(){
     this.apiTareasService.getTareas().subscribe({next:res=>{
       this.tareas = res;
+      this.tareasTotal = res;
       this.cargando = false;
     },
     error: err=>{
@@ -26,7 +32,17 @@ export class InicioComponent {
       this.error = true;
       this.cargando = false;
     }});
-    
+  }
+
+  buscarTarea(event: Event): void {
+    if (event.target instanceof HTMLInputElement) {
+      const buscarAsig = event.target.value.toLowerCase();
+      if (buscarAsig) {
+        this.tareas = this.tareas.filter(tarea => tarea.asignado.toLowerCase().includes(buscarAsig));
+      } else {
+        this.tareas = this.tareasTotal;
+      }
+    }
   }
 
 }
